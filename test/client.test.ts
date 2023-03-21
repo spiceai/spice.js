fd7ba186246f8d156cdd1c7b262d750c591e6326
Luke Kim <80174+lukekim@users.noreply.github.com>
2023-03-21 16:38:56 +0900

    Revert upgrade to Node 18 (Node 16 min requirement) (#37)

--- test/client.test.ts
+++ test/client.test.ts
@@ -165,16 +165,16 @@ test('test latest prices (other currency) works', async () => {
 test('test historical prices works', async () => {
   const prices = await client.getPrices(
     'BTC-USD',
-    new Date('2023-01-01').getTime(),
-    new Date('2023-01-02').getTime(),
+    new Date('2023-01-01').getTime() / 1000,
+    new Date('2023-01-02').getTime() / 1000,
     '1h'
   );
 
   expect(prices).toBeTruthy();
   expect(prices.pair).toEqual('BTC-USD');
   expect(prices.prices.length).toEqual(24);
-  expect(prices.prices[0].timestamp).toEqual('2023-01-01T00:59:00Z');
-  expect(prices.prices[0].price).toEqual(16539.396678151857);
-  expect(prices.prices[23].timestamp).toEqual('2023-01-01T23:59:00Z');
-  expect(prices.prices[23].price).toEqual(16625.08055070908);
+  expect(prices.prices[0].timestamp).toEqual('2023-01-01T01:00:00Z');
+  expect(prices.prices[0].price).toEqual(16538.01454665046);
+  expect(prices.prices[23].timestamp).toEqual('2023-01-02T00:00:00Z');
+  expect(prices.prices[23].price).toEqual(16625.51039411775);
 });
