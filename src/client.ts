6f8a8f5475792cf01b2796433fe40fcfc5953231
peasee <98815791+peasee@users.noreply.github.com>
2024-09-18 20:50:57 +1000

    fix: Use os.machine() instead of os.arch()

--- src/client.ts
+++ src/client.ts
@@ -48,7 +48,7 @@ import { VERSION } from "./version";
 function getUserAgent(): string {
   const os_type = os.type();
   const os_release = os.release();
-  const os_arch = os.arch();
+  const os_arch = os.machine();
   return `spice.js ${VERSION} (${os_type}/${os_release} ${os_arch})`;
 }
 
