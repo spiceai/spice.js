4bd4d511b1528675c2cd238aba7a45612c321b34
peasee <98815791+peasee@users.noreply.github.com>
2024-09-19 09:55:30 +1000

    fix: Remove invalid leading space in regex test

--- test/user-agent.test.ts
+++ test/user-agent.test.ts
@@ -1,7 +1,7 @@
 import { getUserAgent } from "../src/user-agent";
 
 describe("user-agent", () => {
-    const matching_regex = /spice.js \d+\.\d+\.\d+ \((Linux|Windows|Darwin)\/[\d\w\.\-\_]+ (x86_64 | aarch64 | i386 | arm64) \)/;
+    const matching_regex = /spice.js \d+\.\d+\.\d+ \((Linux|Windows|Darwin)\/[\d\w\.\-\_]+ (x86_64 | aarch64 | i386 | arm64)\)/;
 
     it("should match the user-agent regex", async () => {
         const ua = getUserAgent();
